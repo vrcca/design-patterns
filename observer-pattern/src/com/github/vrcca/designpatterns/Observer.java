9cadd009e6bfed1596442936184b69bacdd51d21
Vitor Albuquerque <vitor.rcca@gmail.com>
2016-06-06 23:44:17 -0300

    Observer pattern: added a display as observer

--- observer-pattern/src/com/github/vrcca/designpatterns/Observer.java
+++ observer-pattern/src/com/github/vrcca/designpatterns/Observer.java
@@ -1,5 +1,5 @@
 package com.github.vrcca.designpatterns;
 
 public interface Observer {
-    void update(float temp, float humidity, float pressure);
+    void update(float temperature, float humidity, float pressure);
 }
